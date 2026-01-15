f0f8cc7cb3da08531dc0baf8d4b1ba165678bd4a
Aryan Duhan <Aryanduhan888@gmail.com>
2026-01-16 05:08:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup  
+
+
+
+setup(
+name="mlproject",
+version="0.0.1",
+author="Aryan",
+author_email="Aryanduhan888@gmail.com",
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn'] 
+
+)
+
+
+
+
